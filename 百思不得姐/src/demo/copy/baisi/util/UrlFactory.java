a5b7ac09f4159f7dc3ef55f5f631f5e25f9fb31d
yangkun <1454326822@qq.com>
2016-10-10 18:51:17 +0800

    yangkun

--- 百思不得姐/src/demo/copy/baisi/util/UrlFactory.java
+++ 百思不得姐/src/demo/copy/baisi/util/UrlFactory.java
@@ -15,11 +15,6 @@ public class UrlFactory {
 		return url;
 	}
 
-	public static String getSongURL(String songID) {
-		String url = "http://tingapi.ting.baidu.com/v1/restserver/ting?from=qianqian&version=2.1.0&method=baidu.ting.song.getInfos&format=json&songid="+songID+"&ts=1408284347323&e=JoN56kTXnnbEpd9MVczkYJCSx%2FE1mkLx%2BPMIkTcOEu4%3D&nw=2&ucf=1&res=1";
-		return url;
-	}
-
 	public static String getSearchContent(String content,int page) {
 		try {
 			content = URLEncoder.encode(content, "utf-8");
