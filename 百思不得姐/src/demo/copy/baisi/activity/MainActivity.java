27c48ed0f426e3440a15308b4ebb389ce8a32ac2
Administrator <Administrator@admin-PC>
2016-10-11 09:18:39 +0800

    queyaliang

--- 百思不得姐/src/demo/copy/baisi/activity/MainActivity.java
+++ 百思不得姐/src/demo/copy/baisi/activity/MainActivity.java
@@ -29,6 +29,7 @@ import android.widget.RadioGroup.OnCheckedChangeListener;
 
 @ContentView(R.layout.activity_main)
 public class MainActivity extends FragmentActivity {
+	///////
 	@ViewInject(R.id.vp_content)
 	private ViewPager vpContent;
 	@ViewInject(R.id.rg_bottom_buttons)
